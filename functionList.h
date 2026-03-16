b28d47f781273144538bf5f23dbbc20b53c47ce4
plowh <yanisbelguerras123@gmail.com>
2026-03-16 22:04:12 +0100

    first commit

---
+++ functionList.h
@@ -0,0 +1,4 @@
+#pragma once
+
+std::string getPrefetchFiles();
+void displayPrefetchFiles(const std::string& path);
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+#include "functionList.h"
+
+
+
+int main() {
+
+    std::string path = getPrefetchFiles();
+    displayPrefetchFiles(path);
+
+    system("pause");
+    return 0;
+}
+
+
+std::string getPrefetchFiles() {
+
+  return R"(C:\Windows\Prefetch)"; //defining the path i wanna look thru
+}
+
+void displayPrefetchFiles(const std::string& path) {
+
+    for (const auto& file : std::filesystem::directory_iterator(path)) std::cout << file.path() << std::endl; //printing the files in the path into console
+}
